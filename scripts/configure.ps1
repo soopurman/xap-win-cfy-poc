d70951c8aa94f321213c8817af5c826a2a4c970f
Cloud User <ec2-user@ip-172-31-47-244.us-west-2.compute.internal>
2016-06-08 13:00:14 -0400

    changed POC powershell command

--- scripts/configure.ps1
+++ scripts/configure.ps1
@@ -1,2 +1,2 @@
 $ctx_path=Get-ChildItem -Path $env:USERPROFILE -Filter ctx -Recurse
-$ctx_path instance runtime-properties proof_of_concept success
+$ctx_path deployment id > C:\cfy-deployment-id.txt
