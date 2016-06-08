cd9b46354a4447889674790889780b667d6384e7
Cloud User <ec2-user@ip-172-31-47-244.us-west-2.compute.internal>
2016-06-08 18:04:49 -0400

    increase verbosity

--- scripts/commands.ps1
+++ scripts/commands.ps1
@@ -1,2 +1,9 @@
+echo Begin POC code > C:\Users\Administrator\cfy-deployment.txt
+
+
 $ctx_path=Get-ChildItem -Path $env:USERPROFILE -Filter ctx -Recurse
-&$ctx_path deployment id > C:\Users\Administrator\cfy-deployment-id.txt
+
+&$ctx_path deployment id >> C:\Users\Administrator\cfy-deployment.txt
+
+
+echo End POC code >> C:\Users\Administrator\cfy-deployment.txt
