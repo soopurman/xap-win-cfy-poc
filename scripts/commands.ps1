dee4812b1640e950492a7f8ae43139042b381404
Cloud User <ec2-user@ip-172-31-47-244.us-west-2.compute.internal>
2016-06-13 10:13:13 -0400

    reimplemented deploy script in python, added inputs for URLs to download

--- scripts/commands.ps1
+++
@@ -1,9 +0,0 @@
-echo Begin POC code > C:\Users\Administrator\cfy-deployment.txt
-
-
-$ctx_path=Get-ChildItem -Path $env:USERPROFILE -Filter ctx -Recurse
-
-&$ctx_path deployment id >> C:\Users\Administrator\cfy-deployment.txt
-
-
-echo End POC code >> C:\Users\Administrator\cfy-deployment.txt
---
+++ scripts/deploy.py
@@ -0,0 +1,22 @@
+import sys
+import datetime
+import urllib
+import zipfile
+from cloudify import ctx
+
+fo = open("C:\Users\Administrator\Desktop\cfy-deployment.txt", "a")
+fo.write("Begin POC code\n")
+fo.write(datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + "\n")
+fo.write(ctx.target.instance.id + "\n")
+
+for arg in sys.argv[1:]:
+    zf = "C:\Users\Administrator\Downloads\" + arg.split('/')[-1]
+    fo.write("Downloading " + arg + "\n")
+    urllib.urlretrieve(arg, zf)
+    zip = zipfile.ZipFile(zf)
+    fo.write("Unzipping " + zf + "\n")
+    zip.extractall()
+    zip.close()
+
+fo.write("End POC code\n")
+fo.close()
